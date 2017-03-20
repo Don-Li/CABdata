81f83324bbab0d9d16584393e88d1b4d1a1f9f47
Don-Li <yli877@aucklanduni.ac.nz>
2017-03-20 16:02:54 +1300

    Changed name of vi_1 dataset to variable_interval_1_2017

--- R/variable_interval_1_2017-data.R
+++ R/variable_interval_1_2017-data.R
@@ -1,6 +1,6 @@
-#### vi_1 dataset
+#### variable_interval_1_2017 dataset
 
-#' vi_1 dataset
+#' variable_interval_1_2017 dataset
 #'
 #' Data from a single variable-interval experiment. 11 pigeons for subjects Arranged inter-reinforcement intervals: 30s, 60s, 240s. Reinforced by 2s access to wheat. Session dueation was 1800s.
 #'
@@ -11,7 +11,7 @@
 #' }
 #'
 #' @section{ VI schedule }:{
-#'     A reinforcement delivery is arranged by sampling inter-reinforcement intervals from an Exponential distribution. The arranged inter-reinforcement intervals were: 30s, 60s, and 240s. See the \code{Experiment} and \code{VI} columns.
+#'     A reinforcement delivery is arranged by sampling inter-reinforcement intervals from an Exponential distribution. The arranged inter-reinforcement intervals were: 30s, 60s, and 240s. As an example, for VI30s, the inter-reinforcement intervals were drawn from an Exponential( lambda = 1/30 ) distribution. See the \code{Experiment} and \code{VI} columns.
 #' }
 #'
 #' @section{ Stimuli }:{
@@ -41,7 +41,7 @@
 #'
 #'     A row of three response keys were located on one side of the chamber, 220mm above the floor and 80mm apart (centre to centre). Each key was 25mm in diameter. The stimuli were projected onto these response keys. See \link{http://www.med-associates.com/product/response-key-with-lcd-stimulus-display/}. Only the left key was used for this experiment. 0.1N of force was required to activate the response key.
 #'
-#'     A hopper fillted with wheat was located behind a magazine aperture. The aperture was 80mm above the floor of the experimental chamber. The dimensions of the aperture was 50mm, 60mm, 45mm (height, width, depth). During reinforcement, a light above the magazine was lit and the hopper was raised for the duration of the reinforcement delivery.
+#'     A hopper filled with wheat was located behind a magazine aperture. The aperture was 80mm above the floor of the experimental chamber. The dimensions of the aperture was 50mm, 60mm, 45mm (height, width, depth). During reinforcement, a light above the magazine was lit and the hopper was raised for the duration of the reinforcement delivery.
 #' }
 #'
 #' @section{ Software }:{
@@ -58,6 +58,7 @@
 #'
 #' @seealso
 #' \code{\link[CAB]{import_medpc_to_formal_event_record}} For the function that read the data from the raw files.
+#'
 #' \code{\link[CAB]{event_record}} For the \code{formal_event_record} class.
 #'
 #' @examples
@@ -65,34 +66,34 @@
 #'
 #' file_path = "I:/Data" variable_arrays = list( C = c("resp","rft") )
 #' event_arrays = list( X = c("resp_time" = 1, "rft_time" = 2, "start" = 9 ) )
-#' general_arrays = list( J = "arranged_vi" ) vi_1 =
+#' general_arrays = list( J = "arranged_vi" ) variable_interval_1_2017 =
 #' import_medpc_to_formal_event_record( "Backup", variable_arrays, event_arrays,
 #' general_arrays, file_path = file_path, round = 2 )
 #'
-#' #Fix some bad labels vi_1[ Experiment == "vi_30240", Experiment := "vi_240" ]
-#' vi_1[ MSN == "new_don_vi_60", Experiment := "vi_60" ] #Take out subject 226
-#' and S01 vi_1 = vi_1[ (Subject != 226 & Subject != "S01"), ]
+#' #Fix some bad labels variable_interval_1_2017[ Experiment == "vi_30240", Experiment := "vi_240" ]
+#' variable_interval_1_2017[ MSN == "new_don_vi_60", Experiment := "vi_60" ] #Take out subject 226
+#' and S01 variable_interval_1_2017 = variable_interval_1_2017[ (Subject != 226 & Subject != "S01"), ]
 #'
-#' #Fix column names setnames( vi_1, c("Start Date","End Date" ), c(
-#' "Start_Date", "End_Date" ) ) #Fix the dates vi_1[ , Start_Date := as.Date(
-#' Start_Date, format = "%m/%d/%y" ) ] vi_1[ , End_Date := as.Date( End_Date,
+#' #Fix column names setnames( variable_interval_1_2017, c("Start Date","End Date" ), c(
+#' "Start_Date", "End_Date" ) ) #Fix the dates variable_interval_1_2017[ , Start_Date := as.Date(
+#' Start_Date, format = "%m/%d/%y" ) ] variable_interval_1_2017[ , End_Date := as.Date( End_Date,
 #' format = "%m/%d/%y" ) ]
 #'
 #' #Add numerical values for the VI switch_keys = list( vi_30 = 30, vi_60 = 60,
 #' vi_240 = 240 ) switch_fx = function( value, switch_keys ){ switch_keys = c(
-#' list( EXPR = value ), switch_keys ) do.call( switch, switch_keys ) } vi_1[ ,
+#' list( EXPR = value ), switch_keys ) do.call( switch, switch_keys ) } variable_interval_1_2017[ ,
 #' VI :=  vapply( Experiment, function(x) switch_fx( x, switch_keys ), FUN.VALUE
 # = 1 ) ]
 #'
-#' #Order by subjects, date setorder( vi_1, Subject, Start_Date )
+#' #Order by subjects, date setorder( variable_interval_1_2017, Subject, Start_Date )
 #'
-#' #Add number for vi sessions vi_1[ , VI_session := rep(1:20) ]
+#' #Add number for vi sessions variable_interval_1_2017[ , VI_session := rep(1:20) ]
 #'
-#' #Save the data save( vi_1, file = "vi_1.RData")
+#' #Save the data save( variable_interval_1_2017, file = "variable_interval_1_2017.RData")
 #'
 #' ### Showing small inter-response times and bouncing keys ####
 #' library( CAB )
-#' working_data = copy( vi_1 )
+#' working_data = copy( variable_interval_1_2017 )
 #' working_data[ ,
 #'    c( "resp_rate", "rft_rate") := transpose( lapply( event_record,
 #'        function(x){
@@ -118,9 +119,9 @@
 #' # Note the large group smaller than 0.2s. These are from a combination of "nibbling" responses and bouncing keys.
 #' hist( irts[ Subject == 224, V1 ], breaks = 100000 ], xlim = c(0, 1) )
 #'
-#' @name vi_1
+#' @name variable_interval_1_2017
 #' @docType data
-#' @usage data(vi_1)
+#' @usage data(variable_interval_1_2017)
 #' @format A \code{data.table}.
 #' @keywords variable_interval
 
